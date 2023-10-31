eece66b8ac45d30a4f19d2b95c65d9b26b74c3bc
maseeh <85864840+maseehabdul@users.noreply.github.com>
2023-10-31 12:20:35 +0530

    New upload
    updated

---
+++ dir_fuzz.py
@@ -0,0 +1,42 @@
+import requests
+import argparse
+
+def fuzz():
+    
+        print("----------------------Dir fuzzing tool created by own -------------------------")
+        # Create an ArgumentParser object
+        parser = argparse.ArgumentParser(description="URL and Word Argument Parser")
+
+            # Add the URL argument
+        parser.add_argument("-url", "--url", required=True, help="The URL to fetch")
+
+            # Add the word argument
+        parser.add_argument("-w", "--word", required=True, help="The word to search for")
+
+
+        args = parser.parse_args()
+
+        url = args.url
+        word = args.word
+        
+        with open(word,"r") as file:
+            for line in file:
+                    directory = line.strip()
+                    url1 =f"{url}/{directory}"
+                    
+                                
+                
+                    response = requests.get(url1)
+
+                    # Check the response status code
+                    if response.status_code == 200:
+                            print(f"Directory found: {url1}")
+                    elif response.status_code == 302:
+                            print(f"Not Found: {url1}")
+                        
+                            
+             
+    
+        
+if __name__ == "__main__":
+    fuzz()
